5b4ccdb6ca952a6d8e4316f312226a01d4a3df49
Smita Rosemary <smitarosemary@Smitas-MacBook-Pro.local>
2026-01-30 13:08:13 -0500

    added basic html for both portfolio + art gallery!

---
+++ script.js
@@ -0,0 +1,7 @@
+function revealMessage() {
+    // Get the element with the id 'message'
+    const messageElement = document.getElementById('message');
+    
+    // Change its style to make it visible
+    messageElement.style.display = 'block'; 
+}
